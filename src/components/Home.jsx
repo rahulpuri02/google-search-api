4de72112c288e7c8b36e40bb03f6981cf518acc3
Rahul Puri <rahulxpuri@gmail.com>
2023-04-14 02:22:21 +0530

    working on home-main component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,6 +10,14 @@ const Home = () => {
             <div className='w-full px-5 flex flex-col items-center mt-44 '>
                 <img className='w-[177px] md:w-[272px] mb-8' src={Logo} alt='google-logo' />
                 <SearchInput />
+                <div className='flex gap-2 text-[]#3c4043 mt-8'>
+                    <button className='h-9 px-4 bg-[#f8f9fa] text-sm rounded-md border border-[#f8f9fa] hover:border-[#dadce0] hover:shadow-c2'>
+                        Google Search
+                    </button>
+                    <button className='h-9 px-4 bg-[#f8f9fa] text-sm rounded-md border border-[#f8f9fa] hover:border-[#dadce0] hover:shadow-c2'>
+                        I'm feeling lucky
+                    </button>
+                </div>
             </div>
        
         </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+      extend: {
+          boxShadow: {
+              c: "0 1px 6px rgb(32 33 36 / 28%)",
+              c2: "0 1px 1px rgb(0 0 0 / 10%)",
+          },
+      },
   },
   plugins: [],
-}
+};
